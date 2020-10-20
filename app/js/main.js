18fce78f687007e6e8562ccba54bc0286d460a06
Lavrentev Stanislav <hvoinii@gmail.com>
2020-10-20 22:24:12 +0600

    salt: styles for main-slider, footer; some adaptive; burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,37 @@
 $(function () {
 
-
+  /* main slider */
   $('.main-slider__box').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     prevArrow: false,
     nextArrow: false
   });
-  $('.spotlight__video-preview').magnificPopup({
+  /* video popup */
+  $('.spotlight__video-wrapper').magnificPopup({
     items: {
-         src: 'https://www.youtube.com/watch?v=YltblC65z8E'
-       },
+      src: 'https://www.youtube.com/watch?v=YltblC65z8E'
+    },
     type: 'iframe',
     iframe: {
-          markup: '<div class="mfp-iframe-scaler">'+
-                  '<div class="mfp-close"></div>'+
-                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                  '</div>', 
-          patterns: {
-              youtube: {
-                  index: 'youtube.com/', 
-                  id: 'v=', 
-                  src: '//www.youtube.com/embed/%id%?autoplay=1' 
-              }
-           },
-           srcAction: 'iframe_src', 
-       }
+      markup: '<div class="mfp-iframe-scaler">' +
+        '<div class="mfp-close"></div>' +
+        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+        '</div>',
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: 'v=',
+          src: '//www.youtube.com/embed/%id%?autoplay=1'
+        }
+      },
+      srcAction: 'iframe_src',
+    }
   });
+
+  /* burger */
+  $('.header__burg').on('click',function(){
+    $('.js-nav-list').slideToggle();
+  })
 });
